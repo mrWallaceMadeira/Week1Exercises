f8456755274263d1b6809e17dae3042913546d7d
Mr. Wallace <pwallace@madeira.org>
2020-09-04 08:20:09 -0400

    adding instructions

---
+++ sep4/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+            Exercise 1
+           ------------
+           Write a program that prints a receipt.
+           The receipt should have a border, date, items purchased & name of the store
+           ** Hint: The 'pipe' character (shift + forward-slash) is useful for the side border **
+         */
+
+        // Exercise 1 code below
+
+
+
+        /*
+            Exercise 2
+            ----------
+            Write a program that prints your name, and the position of the vowels in your name
+            ** Hint: https://docs.oracle.com is your friend ** 
+         */
+
+    }
+}
